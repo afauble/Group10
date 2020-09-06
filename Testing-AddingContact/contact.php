50c3a645c36d017f629289715a0afb460a093080
Akash Samlal <triforcemaster345@gmail.com>
2020-09-06 18:47:20 -0400

    Add 'Test-AddContact' files

---
+++ Testing-AddingContact/contact.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Contact</title>
+    <link rel="icon" type="image/png" href="contact.png">
+    <link rel="stylesheet" href="style.css">
+</head>
+<header>
+    <h1>Create a Contact</h1>
+</header>
+
+<body>
+<div class = "CreateContactForm">
+    <ul>
+        <li><input type="text" name="fname" placeholder="First Name"></li>
+        <li><input type="text" name="lname" placeholder="Last Name"></li>
+        <li><input type="text" name="email" placeholder="E-mail Address"></li>
+        <li><input type="text" name="mobilenum" placeholder="Phone-Number"></li>
+        <li><input type="text" name="address" placeholder="Physical Address"></li>
+    </ul>
+    <button type="button">Create Contact</button>
+    <button type="button">Cancel</button>
+</div>
+
+</body>
+</html>
