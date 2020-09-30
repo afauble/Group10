f463056c28e8b4ca3c4921276b57f9331fe58558
Akash Samlal <43329669+AkashSamlal@users.noreply.github.com>
2020-09-30 14:37:56 -0400

    Enable md5 hashing in front end
    When user signup, the hashing is enabled on the front end

--- js/signup.js
+++ js/signup.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
  
+ $(".toggle-password").click(function() {
+     $(this).toggleClass("fa-eye fa-eye-slash");
+     var tog = $($(this).attr("toggle"));
+     
+     if(tog.attr("type") == "password") {
+         tog.attr("type", "text");
+     }
+     else {
+         tog.attr("type", "password");
+     }
+ }); 
+ 
  //When User hits the submit button
   $("#login").on('click', function() {
       var user = $("#username").val();
       var pass = $("#password").val();
-
+      var passConfirm = $("#confirmPassword").val();
+      
     //Username or password is empty -> error
-      if(user === "" || pass === "") {
+      if(user == "" || pass == "" || passConfirm == "") {
           //add error message
           $("#resp").html("Cannot leave an empty username or password");
       }
@@ -14,8 +27,12 @@ $(document).ready(function() {
       else if(user.length > 25) {
         $("#resp").html("Username is too long");
       }
+      else if(pass != passConfirm) {
+         $("#resp").html("Passwords do not match!");
+      }
       //Go to the signup php to insert in database
       else {
+          var hash = md5(pass); 
         $.ajax(
           {
             url: 'LAMPAPI/signup.php',
@@ -23,16 +40,19 @@ $(document).ready(function() {
             data: {
               login: 1,
               userPHP: user,
-              passPHP: pass
+              passPHP: hash
             },
             
             success: function(response) {
+              //If successful go to the login page
+              if(response.indexOf('success') >= 0) {
+                  //Prints back response
+              $("#resp").html("User successfully signed up!");
+                window.location = 'logging.php';
+              }
+              else {
                 //Prints back response
               $("#resp").html(response);
-                
-                //If successful go to the login page
-              if(response.indexOf('success') >= 0) {
-                window.location = 'logging.php';
               }
             },
             dataType: 'text'
