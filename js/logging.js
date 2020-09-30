40a9342b07f504b7985e7c00d0f49b00124ea61f
Akash Samlal <43329669+AkashSamlal@users.noreply.github.com>
2020-09-30 14:38:52 -0400

    Enable md5 hashing in login page

--- js/logging.js
+++ js/logging.js
@@ -12,7 +12,18 @@ $(document).ready(function() {
        $("#password").val(passC);
        $("#remember").prop("checked", true);
     }
-    
+
+$("#pwd").click(function() {
+    $(this).toggleClass("fa-eye fa-eye-slash");
+     var tog = $($(this).attr("toggle"));
+     
+     if(tog.attr("type") == "password") {
+         tog.attr("type", "text");
+     }
+     else {
+         tog.attr("type", "password");
+     } }); 
+     
     //If the login button selected
   $("#login").on('click', function() {
       var user = $("#username").val();
@@ -28,6 +39,7 @@ $(document).ready(function() {
       }
      //Go to the login php to insert in database
       else {
+         var hash = md5(pass);
         $.ajax(
           {
             url: 'LAMPAPI/login.php',
@@ -35,7 +47,7 @@ $(document).ready(function() {
             data: {
               login: 1,
               userPHP: user,
-              passPHP: pass
+              passPHP: hash
             },
             success: function(response) {
              //Prints back response
@@ -43,7 +55,8 @@ $(document).ready(function() {
             
              //If successful go to the directed page
               if(response.indexOf('success') >= 0) {
-                  
+                  //Prints back response
+                $("#resp").html("User login successfully!");
                   //Check if remember me box is selected
                   if($('#remember').is(':checked')) {
                     //Create 3 cookies username, password, remember
@@ -59,11 +72,16 @@ $(document).ready(function() {
                     }
                 window.location = 'directed.php';
               }
+              else {
+                  //Prints back response
+              $("#resp").html(response);
+              }
             },
             dataType: 'text'
           }
         );
       }
   });
+    
 });
 
